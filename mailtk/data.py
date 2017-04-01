ab515255f8fa77b1cbfedf0492612418e28fa74c
Mathias Rav <rav@cs.au.dk>
2017-04-01 12:56:45 -0400

    Try to document the weird thing that is going on

--- mailtk/data.py
+++ mailtk/data.py
@@ -1,3 +1,14 @@
+'''
+Note that subclassing of namedtuple works in a particular duck-typy fashion.
+The base class Mailbox is subclassed as MailboxAccount and MailboxImap.
+The IMAP account returns MailboxImap instances which are wrapped into
+MailboxAccount instances by the controller, and the controller unwraps the
+MailboxAccount into a MailboxImap when passing back to the account.
+This works even though MailboxAccount subclasses Mailbox instead of
+MailboxImap.
+'''
+
+
 from mailtk.namedtuple_with_abc import namedtuple
 
 
