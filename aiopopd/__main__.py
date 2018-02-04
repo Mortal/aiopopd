b6ab952f36c6345cd0f3bacbb1ef2d7bd8728164
Mathias Rav <m@git.strova.dk>
2018-02-04 15:15:27 +0100

    main: Use Controller and add SSL support

--- aiopopd/__main__.py
+++ aiopopd/__main__.py
@@ -1,8 +1,11 @@
-import asyncio
+import os
+import ssl
 import logging
 import argparse
+import subprocess
 from aiopopd.pop import Pop3
 from aiopopd.imap import ImapHandler
+from aiopopd.controller import Controller
 
 
 parser = argparse.ArgumentParser()
@@ -10,33 +13,53 @@ parser.add_argument('-H', '--imap-hostname', required=True)
 parser.add_argument('-p', '--imap-port', required=True, type=int)
 parser.add_argument('-s', '--imap-ssl', action='store_true')
 parser.add_argument('-P', '--listen-port', required=True, type=int)
-HOST = '127.0.0.1'
+parser.add_argument('--ssl-key')
+parser.add_argument('--ssl-cert')
+parser.add_argument('--ssl-generate', action='store_true')
+
+
+def get_ssl_context(args):
+    if not args.ssl_key and not args.ssl_cert:
+        return None
+    if not args.ssl_key or not args.ssl_cert:
+        raise SystemExit('Both --ssl-key and --ssl-cert must be specified')
+
+    # Create cert and key if they don't exist
+    generate = (args.ssl_generate and
+                not os.path.exists(args.ssl_cert) and
+                not os.path.exists(args.ssl_key))
+
+    if generate:
+        subprocess.check_call(
+            ['openssl', 'req', '-x509', '-newkey', 'rsa:4096',
+             '-keyout', args.ssl_key, '-out', args.ssl_cert, '-days', '365',
+             '-nodes', '-subj', '/CN=localhost'])
+
+    # Load SSL context
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.load_cert_chain(args.ssl_cert, args.ssl_key)
+    return context
 
 
 def main():
     args = parser.parse_args()
+    ssl_context = get_ssl_context(args)
     logging.basicConfig(level=logging.ERROR)
     log = logging.getLogger('aiopopd.log')
     log.setLevel(logging.DEBUG)
-    loop = asyncio.get_event_loop()
-    loop.set_debug(enabled=True)
 
     def factory():
         return Pop3(ImapHandler(args.imap_hostname,
                                 args.imap_port,
                                 args.imap_ssl))
 
-    server = loop.run_until_complete(
-        loop.create_server(factory, host=HOST, port=args.listen_port))
-    log.info('Starting asyncio loop')
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        pass
-    server.close()
-    log.info('Completed asyncio loop')
-    loop.run_until_complete(server.wait_closed())
-    loop.close()
+    controller = Controller(None, port=args.listen_port, ssl_context=ssl_context)
+    controller.factory = factory
+    controller.loop.set_debug(enabled=True)
+    controller.start()
+    print('Server started; press Return to stop')
+    input('')
+    controller.stop()
 
 
 if __name__ == '__main__':
